a7b3cd4f9563022079a0f35345351511433bb2fd
sharma0911 <sharma09112000@gmail.com>
2025-05-03 15:46:54 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Enable environment variables
+  env: {
+    PERISKOPE_SIGNING_SECRET: process.env.PERISKOPE_SIGNING_SECRET,
+  },
+  // Disable strict mode for simplicity
+  reactStrictMode: false,
+}
